c4fd66b3c65b8293c7b2be06deea5fe3ae625faa
ronina24 <roni24682@gmail.com>
2022-11-30 01:22:18 +0200

    login router+controller and validate users

---
+++ controllers/login_controller.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcrypt');
+const users= require('../data/users.json');
+
+
+const getUserByEmail = (email)=>{
+    const user = users.find(user => user.email === email);
+    return user ? user : "User does'nt exist";
+}
+
+function isAfter(date1, date2) {
+    return date1 > date2;
+}
+
+const isSuspend = (user)=> {
+    if (user.suspensionTime == '0' && user.suspensionDate == null && user.status != 'suspended') {
+        console.log(`user: ${user["email"]} is not suspended- login succeeded`);
+        return 0;
+    }
+    const today = new Date();
+    const suspendTime = user.suspensionTime;
+    const suspendStartDate = user.suspensionDate;
+    const isSuspend = isAfter(suspendStartDate + parseInt(suspendTime), today);
+    if (isSuspend) {
+        console.log(`user: ${user["email"]} is suspended- login failed`, 'ERROR');
+        return suspendStartDate + parseInt(suspendTime);
+    }
+}
+
+module.exports = {getUserByEmail,isSuspend}
+
+
+
+
+/*const usrs = require('cata/users.json');*/
+/*const validateUser = (req,res) => {
+    try {
+        const user = getUserByEmail(req.body.email);
+        if (user==null){
+            throw new ReferenceError('No user with that mail');
+        }
+        //const hashedPass = bcrypt.hash(req.body.password,10);
+        if (await bcrypt.compare(req.body.password,user.password)){
+
+        }else {
+            throw new Error('Incorrect password');
+        }
+
+    } catch(e){
+        console.log(e.message);
+    }
+}*/
+
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
+bodyParser = require('body-parser');
+const login_route =  require('./login_route')
 
-const {login} = require("./routers/login_route");
-const {signUp} = require("./routers/signUp_route");
+/*const {signUp} = require("./routers/signUp_route");
 const {forgotPassword} = require("./routers/forgotPassword_route");
 const {suspend} = require("./routers/suspend_route");
 const {changePassword} = require("./routers/changePassword_route");
 const {confirmCode} = require("./routers/confirmCode_refactor");
-const {adminCRUD} = require("./routers/adminCRUD_route");
+const {adminCRUD} = require("./routers/adminCRUD_route");*/
 
 
-app.use('/api/login', login);
-app.use('/api/signUp', signUp);
+app.use('/login', login_route.loginRouter);
+
+/*app.use('/api/signUp', signUp);
 app.use('/api/forgotPassword', forgotPassword);
 app.use('/api/suspend', suspend);
 app.use('/api/changePassword', changePassword);
@@ -19,7 +21,6 @@ app.use('/api/confirmCode', confirmCode);
 app.use('/api/adminCRUD', adminCRUD);
 app.use((req, res) => {
     res.status(400).send('Something is broken!');
-});
-
+});*/
 
 module.exports = {app}
