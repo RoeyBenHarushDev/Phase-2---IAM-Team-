468d26992372e531fe835f0e2ad173fe10de968f
Shahar <shaharariel95@gmail.com>
2022-12-03 17:26:30 +0200

    Working on Sending an HTML file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { app } = require("./routers/mainRouter");
 const logger = require("morgan"); // NOTE: for debugging
 const port = process.env.PORT || 3000;
-
 // app.use(express.json());
 // app.use(express.urlencoded({extended: true}));
 // app.use(logger("dev"));
 
-app.listen(port, () => console.log(`Express server is running on port ${port}`));
+app.listen(port,() => console.log(`Express server is running on port ${port}`));
+
 
