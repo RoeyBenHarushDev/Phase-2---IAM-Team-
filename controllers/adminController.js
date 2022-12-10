2b832d05c0ddc3a9e43f29e0c9247b6db6051308
dkracheli <83089578+dkracheli@users.noreply.github.com>
2022-12-11 01:43:32 +0200

    bug

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const dbHandler = require("../data/dbHandler")
 const adminService = require("../services/adminService");
 const User = require("../models/users");
 const OTP = require("../models/OTPPass");
+const jwt = require("jsonwebtoken");
 
 
 async function handleAddUser(req, res) {
@@ -22,13 +23,18 @@ async function handleAddUser(req, res) {
 
 async function handleShowAllUsers(req, res) {
     const showAllUser = await User.find({});
-    return res.send(JSON.stringify(showAllUser));
+    return res.send(showAllUser);
 }
 
 async function handleDeleteUser(req, res) {
     try {
-        await User.findOneAndDelete({'email': req.body.email});
-        await OTP.findOneAndDelete({'email': req.body.email});
+        const token = req.cookies.token;
+        const userObj = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET).user;
+        const email = req.body.email;
+        if(email === userObj.email){ throw new Error("Unable to delete yourself")}
+
+        await User.findOneAndDelete({'email': email});
+        await OTP.findOneAndDelete({'email': email});
         return res.status(200).json({message: "The user has been deleted"})
     } catch (e) {
         return res.status(401).json({message: e.message})
@@ -48,6 +54,7 @@ async function handleSaveUpdate(req, res) {
             const user = await dbHandler.getUserByEmail(req.body.email);
             await adminService.ifClosed(user);
             const updeteUser = adminService.changeUserStatus(user,req.body);
+
             await dbHandler.updateUser(req.body.email, updeteUser);
             return res.status(200).json({message: "user update"})
         } else {
