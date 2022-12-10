a390aa553f12abcd9b25da7913dbbc873ed0cd39
ronina24 <roni24682@gmail.com>
2022-12-11 00:27:05 +0200

    postman api check and corrections

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,5 +1,6 @@
 const signUp = require("../services/signUpService");
 const dbHandler = require('../data/dbHandler');
+const mailerAPI = require("../growth/mailerAPI");
 
 async function handleSignUp(req, res) {
     try {
@@ -9,9 +10,11 @@ async function handleSignUp(req, res) {
         if(findUser){
             throw new Error("user already exists");
         }
-        await signUp.userExist(user.email)
-        await signUp.sendEmail(req.body)
-        return res.status(200).json({"message": "code has been sent"});
+        await signUp.userExist(user.email);
+        await signUp.sendEmail(req.body);
+        // Growth team API(mailerAPI) - sending email after user register.
+        await mailerAPI.sendMail(user.email, 'welcome',' <h1>Welcome !</h1>');
+        return res.status(200).json({message: "code has been sent"});
 
 
     } catch (e) {
